6ae994ea973313dcd1260b2340dff4e45dbc923e
jooho-le <wngh0606@naver.com>
2025-10-31 18:01:01 +0900

    end

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: '/vue-static-page/' // 레포 이름으로 지정!
+    base: '/vue-static-page/'
 })
